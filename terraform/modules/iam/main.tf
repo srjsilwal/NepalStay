984445cfed53a1ba4ee150d08225a7d657eeb095
srjsilwal <s6007118@gmail.com>
2026-08-12 21:51:21 +0545

    feat: add Kubernetes manifests for nepalstay application including deployment, service, ingress, HPA, and network policies

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -184,3 +184,4 @@ resource "aws_iam_role_policy_attachment" "github_eks_readonly" {
   role       = aws_iam_role.github_actions.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSReadOnlyAccess"
 }
+
