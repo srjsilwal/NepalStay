0eea93c7fbf93f1fa4cad6715a20150b3f127a3c
srjsilwal <s6007118@gmail.com>
2026-08-25 11:25:20 +0545

    fix: adjust EKS node group scaling configuration for optimal resource management

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -57,9 +57,9 @@ resource "aws_eks_node_group" "this" {
   ]
 
   scaling_config {
-    desired_size = 2
-    min_size     = 1
-    max_size     = 3
+    desired_size = 3
+    min_size     = 2
+    max_size     = 4
   }
 
   update_config {
